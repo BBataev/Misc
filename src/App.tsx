b00c61c1962f73b081bb4f503837bedc18e6d688
Bakar <nightsave4268@gmail.com>
2024-05-07 17:23:36 +0300

    tracker is ready but theres bug must be fixed soon

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,24 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./app.css";
 import { MainPage, UserInfoPage, UsersPage } from "./pages";
 import { Playlists } from "./pages/PlaylistsPage/PlaylistsPage";
+import { useEffect, useState } from "react";
 
 export function App() {
+	const [active, setActive] = useState([false, false, false]);
+
+    useEffect(() => {
+        const savedActiveState = JSON.parse(localStorage.getItem("activeLinks") || "null");
+        if (savedActiveState) {
+            setActive(savedActiveState);
+        }
+    }, []);
+
+	const handleLinkClick = (index: number) => {
+		const newActiveLinks = active.map((_, i) => i === index);
+		setActive(newActiveLinks);
+		localStorage.setItem("activeLinks", JSON.stringify(newActiveLinks));
+	  };
+
 	return (
 		<BrowserRouter>
 			<div className="header">
@@ -12,9 +28,10 @@ export function App() {
 
 			<div className="section">
 				<nav className="navMenu">
-					<Link className="navMenu__link" to={"/"}>Main</Link>
-					<Link className="navMenu__link" to={"/users"}>Users</Link>
-					<Link className="navMenu__link" to={"/playlists"}>Playlists</Link>
+					<Link className={active[0] ? "navMenu__link active" : "navMenu__link"} onClick={() => {handleLinkClick(0)}} to={"/"}>Main</Link>
+					<Link className={active[1] ? "navMenu__link active" : "navMenu__link"} onClick={() => {handleLinkClick(1)}} to={"/users"}>Users</Link>
+					<Link className={active[2] ? "navMenu__link active" : "navMenu__link"} onClick={() => {handleLinkClick(2)}} to={"/playlists"}>Playlists</Link>
+					<div className="navMenu__indicator"></div>
 				</nav>
 
 				<main className="content">
