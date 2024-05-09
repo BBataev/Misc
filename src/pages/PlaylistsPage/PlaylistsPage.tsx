4f279791c3c9d305babe20e8812a166bea399e83
Bakar <nightsave4268@gmail.com>
2024-05-09 19:54:57 +0300

    new page created; only playlist page left and fix some little problems

--- src/pages/PlaylistsPage/PlaylistsPage.tsx
+++ src/pages/PlaylistsPage/PlaylistsPage.tsx
@@ -1,3 +1,67 @@
+import { ChangeEvent } from "react";
+import { PLAYLISTS } from "../../data";
+import { Link, useSearchParams } from "react-router-dom";
+
+import "./PlaylistsPage.css"
+
 export const Playlists = () => {
-  return <h1>porno</h1>;
+  const [searchNameParam, setSearchNameParam] = useSearchParams();
+  const [searchGenreParam, setSearchGenreParam] = useSearchParams();
+
+  const handleSearchName = (event: ChangeEvent<HTMLInputElement>): void => {
+    const { value } = event.target;
+    setSearchNameParam({ searchName: value.toLowerCase() });
+  };
+
+  const handleSearchGenre = (event: ChangeEvent<HTMLInputElement>): void => {
+    const { value } = event.target;
+    setSearchGenreParam({ searchGenre: value.toLowerCase() });
+  };
+
+
+  const searchName = searchNameParam.get("searchName") || "";
+  const searchGenre = searchGenreParam.get("searchGenre") || "";
+
+  const filteredPlaylists = PLAYLISTS.filter(({ name, genre }) =>
+    name.toLowerCase().includes(searchName) && genre.toLowerCase().includes(searchGenre) && (genre !== "Non Music" && name !== "")
+  );
+
+  return (
+    <div className="playlistsPage">
+      <div className="usersPage__startInfo">
+        <h2 className="startInfo__title">Playlists</h2>
+        <label className="startInfo__search">
+          <h4 className="search__title">Fast Search</h4>
+          <div className="search-filters">
+            <input
+              className="filters__input"
+              type="text"
+              value={searchName}
+              onChange={handleSearchName}
+              placeholder="Enter name"
+            />
+            <input
+              className="filters__input"
+              type="text"
+              value={searchGenre}
+              onChange={handleSearchGenre}
+              placeholder="Enter genre"
+            />
+          </div>
+        </label>
+      </div>
+      <div className="playlistsPage-playlists">
+      {
+        filteredPlaylists.map(({id, name }) => (
+          <Link className="playlists__playlist" to={`/playlists/${id}`} key={id}>
+            <h3 className="playlist__name">
+              {name}
+            </h3>
+          </Link>
+        ))
+      }
+      </div>
+
+    </div>
+  );
 };
